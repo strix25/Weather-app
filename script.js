415874dbeb1a03d500d23844e3b2cb2a005bb73a
strix25 <damjan.oslaj@gmail.com>
2017-12-09 13:12:37 +0100

    +GeoLocation
    .

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var x = document.getElementById("demo");
+function geo_success(position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+    //do_something(position.coords.latitude, position.coords.longitude);
+  }
+  
+  function geo_error() {
+    console.log("Sorry, no position available.");
+  }
+  
+  var geo_options = {
+    enableHighAccuracy: true, 
+    maximumAge        : 30000, 
+    timeout           : 27000
+  };
+  
+  var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
-function getLocation() {
-    if (navigator.geolocation) {
-        console.log(navigator.geolocation.getCurrentPosition(showPosition));
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
