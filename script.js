91a91c10176dc48f20aa6a94607a85f8cbbc8c5d
strix25 <damjan.oslaj@gmail.com>
2017-12-10 16:29:56 +0100

    .
    .

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const locationn = document.getElementById("location");
+const temp = document.getElementById("temp");
+const weather = document.getElementById("weather");
+const icon = document.getElementById("icon");
+
 function geo_success(position) {
     console.log(position.coords.latitude, position.coords.longitude);
     //do_something(position.coords.latitude, position.coords.longitude);
@@ -19,15 +24,23 @@ function geo_success(position) {
 
 // //ajax req
 function potato(lat, long){
-    console.log("hi");
-    let link = `https://fcc-weather-api.glitch.me/api/current?lat=46.5546503&lon=15.6458812`;
-    //let link = `https://fcc-weather-api.glitch.me/api/current?lon=:${long}&lat=:${lat}`;
+    console.log(typeof(long));
+
+    //let link = `https://fcc-weather-api.glitch.me/api/current?lat=46.5546503&lon=15.6458812`;
+    let link = `https://fcc-weather-api.glitch.me/api/current?lon=${long}&lat=${lat}`;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', link);
     xhr.onload = function() {
         if (this.status === 200) {
-            console.log(xhr.responseText);
-            console.log(xhr.response);
+            var data = JSON.parse(xhr.responseText);
+
+            locationn.innerHTML = data.name;
+            let deg = data.main.temp *10;
+            temp.innerHTML = Math.round(deg)/10;
+            weather.innerHTML = data.weather[0].main;
+            icon.src = data.weather[0].icon;
+
+            console.log(data);
             console.log("first part");
         }
         else {
@@ -38,17 +51,17 @@ function potato(lat, long){
     xhr.send(null);
 }
 
-// function potato(){
-//     let url = `https://fcc-weather-api.glitch.me/api/current?lat=46.5546503&lon=15.6458812`;
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', url, true);
 
-//     console.log(xhr.readyState);
 
-//     xhr.onload = function (){
-//         console.log("READYstate: ", xhr.readyState);
-//         if(this.status == 200){
-//             console.log("aaaaaaaa");
-//         }
+// ​$("#tempunit").click(function () {
+//     var currentTempUnit = $("#tempunit").text();
+//     var newTempUnit = currentTempUnit == "C" ? "F" : "C";
+//     $("#tempunit").text(newTempUnit);
+//     if (newTempUnit == "F") {
+//       var fahTemp = Math.round(parseInt($("#temp").text()) * 9 / 5 + 32);
+//       $("#temp").text(fahTemp + " " + String.fromCharCode(176));
+//     } else {
+//       $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
 //     }
-// }
+//   });
+  
